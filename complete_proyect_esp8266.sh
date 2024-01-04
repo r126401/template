031b3513b01c801b4fbcbf1b9921c7ef7306ed18
r126401 <r126401@gmail.com>
2024-01-04 21:01:11 +0100

    dd

--- complete_proyect_esp8266.sh
+++ complete_proyect_esp8266.sh
@@ -1,5 +1,39 @@
 #!/bin/sh
 
+device=$1
+
+echo el dispositivo es $device
+
+if [ "$1" = "esp8266" ]; then
+
+echo "cp sdkconfig_esp8266 sdkconfig"
+cp sdkconfig_esp8266 sdkconfig
+
+else
+
+
+if [ "$1" = "esp32" ]; then
+
+echo "cp sdkconfig_esp32 sdkconfig"
+cp sdkconfig_esp32 sdkconfig
+else
+
+
+if [ "$1" = "esp32s3" ]; then
+
+echo "cp sdkconfig_esp32s3 sdkconfig"
+cp sdkconfig_esp32s3 sdkconfig
+
+else
+
+echo "Error al elegir el dispositivo"
+return
+
+fi
+fi
+fi
+
+
 echo "Desconectando el proyecto del template"
 git remote remove origin
 echo "Importando componentes"
@@ -12,6 +46,7 @@ echo "copiando el sdkconfig"
 cp sdkconfig_esp8266 sdkconfig
 git init
 git add .
+
 
 
 
