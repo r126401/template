e406fe2e9826b22bf85fb8f67690580b1a09cd59
r126401 <r126401@gmail.com>
2024-01-04 18:59:03 +0100

    dd

--- complete_proyect_esp8266.sh
+++ complete_proyect_esp8266.sh
@@ -1,8 +1,11 @@
-!#/bin/sh
-
+#!/bin/sh
+echo "Importando componentes"
 git submodule add git@github.com:r126401/components.git
 cd components
+echo "importando user_interface
 git submodule add git@github.com:r126401/user_interface.git
 cd ..
+echo "copiando el sdkconfig"
 cp sdkconfig_esp8266 sdkconfig
+
 
